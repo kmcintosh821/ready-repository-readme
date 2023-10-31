a202d274bd3506ce03de22a2017881fd14353b89
kmcintosh821 <tmcintosh821@gmail.com>
2023-10-31 12:13:02 -0400

    Finished filewriting functions

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('generateMarkdown');
 const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [{
     name: 'title',
-    message: 'Provide a title for your project.',
+    message: 'Provide a title for your project.\n',
     type: 'input'
 },{
     name: 'description',
-    message: 'Provide a short description explaining your motivations for this project, and what you learned.',
+    message: 'Provide a short description explaining your motivations for this project, and what you learned.\n',
     type: 'input'
 },{
     name: 'installation',
-    message: 'Provide the steps for installing this project.',
+    message: 'Provide the steps for installing this project.\n',
     type: 'input'
 },{
     name: 'usage',
-    message: 'Provide instructions for use. (If you want to provide screenshots, those must be inserted after the initial markdown is generated.)',
+    message: 'Provide instructions for use. (If you want to provide screenshots, those must be inserted after the initial markdown is generated.)\n',
+    type: 'input'
+},{
+    name: 'tests',
+    message: 'Provide instructions for any applicable tests.\n',
+    type: 'input'
+},{
+    name: 'contribution',
+    message: 'What guidelines do you want to give to any future contributors?\n',
     type: 'input'
 },{
     name: 'credits',
-    message: 'List any collaborators, third-party assets, or tutorials you used.',
+    message: 'List any collaborators, third-party assets, or tutorials you used.\n',
     type: 'input'
 },{
     name: 'license',
-    message: 'What license did you use for this project? (List provided by GitHub)',
+    message: 'What license did you use for this project? (List provided by GitHub)\n',
     type: 'list',
     choices: [
         'None',
@@ -39,17 +48,35 @@ const questions = [{
         'Eclipse Public License 2.0',
         'GNU Affero General Public License v3.0',
         'GNU General Public License v2.0',
-        'GNU Lesser General Public License v2.1',
+        'GNU Lesser General Public License v3.0',
         'Mozilla Public License 2.0',
         'The Unlicense'
     ]
-}];
+},{
+    name: 'username',
+    message: 'Enter your GitHub username.\n',
+    type: 'input'
+},{
+    name: 'email',
+    message: 'Enter an email address for readers to contact you with.\n',
+    type: 'input'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    markdown = generateMarkdown(data)
+    fs.writeFile('./destination/README.md', markdown, (err) => {
+        if (err) throw err;
+    })
+    console.log("README successfully created!")
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const data = await inquirer.prompt(questions)
+    writeToFile(data)
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,132 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache License 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    case 'GNU General Public License v3.0':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    case 'MIT License':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+
+    case 'BSD 2-Clause "Simplified" License':
+      return '![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)';
+
+    case 'BSD 3-Clause "New" or "Revised" License':
+      return '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
+
+    case 'Boost Software License 1.0':
+      return '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
+
+    case 'Creative Commons Zero v1.0 Universal':
+      return '![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)';
+
+    case 'Eclipse Public License 2.0':
+      return '![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)';
+
+    case 'GNU Affero General Public License v3.0':
+      return '![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)';
+
+    case 'GNU General Public License v2.0':
+      return '![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+
+    case 'GNU Lesser General Public License v3.0':
+      return '![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)';
+
+    case 'Mozilla Public License 2.0':
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)';
+
+    case 'The Unlicense':
+      return '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+  }
+}
+
+function renderLicenseSection(license) {
+  let licenseLink;
+  switch (license) {
+    case 'Apache License 2.0':
+      licenseLink = '(https://opensource.org/licenses/Apache-2.0)';
+
+    case 'GNU General Public License v3.0':
+      licenseLink = '(https://www.gnu.org/licenses/gpl-3.0)';
+
+    case 'MIT License':
+      licenseLink = '(https://opensource.org/licenses/MIT)';
+
+    case 'BSD 2-Clause "Simplified" License':
+      licenseLink = '(https://opensource.org/licenses/BSD-2-Clause)';
+
+    case 'BSD 3-Clause "New" or "Revised" License':
+      licenseLink = '(https://opensource.org/licenses/BSD-3-Clause)';
+
+    case 'Boost Software License 1.0':
+      licenseLink = '(https://www.boost.org/LICENSE_1_0.txt)';
+
+    case 'Creative Commons Zero v1.0 Universal':
+      licenseLink = '(http://creativecommons.org/publicdomain/zero/1.0/)';
+
+    case 'Eclipse Public License 2.0':
+      licenseLink = '(https://opensource.org/licenses/EPL-1.0)';
+
+    case 'GNU Affero General Public License v3.0':
+      licenseLink = '(https://www.gnu.org/licenses/agpl-3.0)';
+
+    case 'GNU General Public License v2.0':
+      licenseLink = '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+
+    case 'GNU Lesser General Public License v3.0':
+      licenseLink = '(https://www.gnu.org/licenses/lgpl-3.0)';
+
+    case 'Mozilla Public License 2.0':
+      licenseLink = '(https://opensource.org/licenses/MPL-2.0)';
+
+    case 'The Unlicense':
+      licenseLink = '(http://unlicense.org/)';
+  }
+  if (licenseLink.length)
+    return `This repository uses the ${license} ${licenseLink}`
+  else return `This repository does not have a license associated with it.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  return `# ${data.title}\n
+  ${licenseBadge}\n
+  ## Table of Contents\n
+- [Description](#description)\n
+- [Installation](#installation)\n
+- [Usage](#usage)\n
+- [Testing Instructions](#testing-instructions)\n
+- [Contribution](#contribution)\n
+- [Credits](#credits)\n
+- [License](#license)\n
+- [Questions](#questions)\n
 
-`;
+  ## Description\n
+  ${data.description}\n
+  \n
+  ## Installation\n
+  ${data.installation}\n
+  \n
+  ## Usage\n
+  ${data.usage}\n
+  \n
+  ## Testing Instructions\n
+  ${data.tests}\n
+  \n
+  ## Contribution\n
+  ${data.contribution}\n
+  \n
+  ## Credits\n
+  ${data.credits}\n
+  \n
+  ## License\n
+  ${licenseSection}\n
+  \n
+  ## Questions\n
+  For any questions, contact ${data.username} through GitHub (https://github.com/${data.username}) or email at ${data.email}`;
 }
 
 module.exports = generateMarkdown;
